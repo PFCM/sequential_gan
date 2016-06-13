1289c079f77bea07bcf6f96c9c96cf38820a1e26
Paul Mathews <pfcmathews@gmail.com>
2016-06-13 22:39:18 +1200

    seems to create a model

--- gen_adv.py
+++ gen_adv.py
@@ -3,9 +3,34 @@ import numpy as np
 import tensorflow as tf
 
 
+class new_collection(object):
+    """decorator that runs a function and adds any variables it adds to
+    TRAINABLE_VARIABLES to a new collection with specified name."""
+
+    def __init__(self, name):
+        self.name = name
+
+    def __call__(self, func):
+        def _wrap(*args, **kwargs):
+            initial_tvars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+            # do it
+            retval = func(*args, **kwargs)
+            # see what it added
+            new_tvars = [var for var
+                         in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+                         if var not in initial_tvars]
+            for var in new_tvars:
+                tf.add_to_collection(self.name, var)
+            return retval  # in case, probably nothing
+        return _wrap
+
+
+@new_collection('discriminator')
 def discriminative_model(inputs, num_layers, width, classifier_shape,
                          embedding_matrix, sequence_lengths):
-    """Gets the discriminator"""
+    """Gets the discriminator. Puts the variabls into a collection called
+    'discriminator'.
+    """
     initial_state, outputs, final_state = _recurrent_model(
         inputs, num_layers, width, batch_size, sequence_lengths,
         embedding_matrix, feed_previous=False)
@@ -17,7 +42,7 @@ def discriminative_model(inputs, num_layers, width, classifier_shape,
     return layer_input
 
 
-def _ff_layer(in_var, size, nonlin=tf.nn.relu, name='layer'):
+def _ff_layer(in_var, size, nonlin=tf.nn.relu, name='layer', collections=None):
     """Gets a feed forward layer."""
     weights = tf.get_variable(name+'_W', shape=[in_var.get_shape()[1].value,
                                                 size],
@@ -27,10 +52,12 @@ def _ff_layer(in_var, size, nonlin=tf.nn.relu, name='layer'):
     return nonlin(tf.nn.bias_add(tf.matmul(inputs, size), biases))
 
 
+@new_collection('generator')
 def generative_model(inputs, num_layers, width, embedding_matrix):
     """Gets the generative part of the model. Creates a sequence of from some
-    kind of initialisation (probably noise).
-    Returns the list of output tensors.
+    kind of initialisation (probably noise). Puts the variables into
+    a collections called 'generator'.
+    Returns the list of output tensors for now, may need more in the future.
     """
     batch_size = inputs[0].get_shape()[0].value
     initial_state, outputs, final_state = _recurrent_model(
@@ -50,21 +77,22 @@ def _recurrent_model(inputs, num_layers, width,
         width: how many units in each of the layers.
         batch_size: how many to do at a time.
         sequence_lengths: a batch_size vector if ints which indicates
-        how long each sequence is, ignored if feed_previous is true.
+          how long each sequence is, ignored if feed_previous is true.
     """
-    cell = tf.nn.rnn_cell.BasicLSTMCell(width)
+    cell = tf.nn.rnn_cell.LSTMCell(width)
     if num_layers > 1:
         cell = tf.nn.rnn_cell.MultiRNNCell([cell] * num_layers)
 
     # get real inputs
-
-    inputs = [tf.nn.embedding_lookup(embedding_matrix, input_)
+    inputs = [tf.nn.embedding_lookup([embedding_matrix], input_)
               for input_ in inputs]
-    initial_state = cell.zero_state(batch_size)
+    initial_state = cell.zero_state(batch_size, tf.float32)
     if feed_previous:
         outputs, final_state = tf.nn.seq2seq.rnn_decoder(
             inputs, initial_state, cell,
-            loop_function=lambda prev, i: tf.argmax(prev))  # could sample?
+            loop_function=lambda prev, i: tf.nn.embedding_lookup(
+                [embedding_matrix],
+                tf.argmax(prev, 1)))  # could sample?
     else:
         outputs, final_state = tf.nn.rnn(
             cell, inputs, initial_state=initial_state,
@@ -73,4 +101,28 @@ def _recurrent_model(inputs, num_layers, width,
 
 
 if __name__ == '__main__':
-    real_seq_inputs = [tf.placeholder(tf.int64)]
+    import string
+    # quick test
+    batch_size = 10
+    seq_len = 15
+    num_symbols = len(string.ascii_lowercase)
+
+    # make both nets the same for now
+    num_layers = 2
+    layer_width = 10
+
+    # need some random integers
+    noise_var = [tf.random_uniform(
+        [batch_size], maxval=num_symbols, dtype=tf.int32)] * seq_len
+
+    embedding = tf.get_variable('embedding', shape=[num_symbols, layer_width])
+    generator_outputs = generative_model(
+        noise_var, num_layers, layer_width, embedding)
+    argmax_outputs = [tf.argmax(output, 1) for output in generator_outputs]
+    sess = tf.Session()
+    with sess.as_default():
+        sess.run(tf.initialize_all_variables())
+        outs = sess.run(argmax_outputs)
+
+        symbols = [string.ascii_lowercase[i[0]] for i in outs]
+        print(''.join(symbols))

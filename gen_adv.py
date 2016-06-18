45ccb32743634c73ba7ceab7a5d2c3674c155c69
Paul Mathews <pfcmathews@gmail.com>
2016-06-18 17:16:16 +1200

    actually seems to do stuff -- had to use more of a policy gradient approach to train the generator through the sampler, but that seems perfectly reasonable

--- gen_adv.py
+++ gen_adv.py
@@ -1,4 +1,15 @@
-"""generative adversarial?"""
+"""generative adversarial?
+
+On sequences. The challenge is:
+    we want to sample a sequence according to the generator (probably)
+    this is going to be hard to backpropagate
+        solutions a) treat is as RL, give the generator an appropriate reward
+                     back it up like a policy gradient
+                  b) come up with some continuous letter representation which
+                     we can pass through directly.
+
+    for now we are going with a).
+"""
 import numpy as np
 import tensorflow as tf
 
@@ -21,49 +32,56 @@ class new_collection(object):
                          if var not in initial_tvars]
             for var in new_tvars:
                 tf.add_to_collection(self.name, var)
-            return retval  # in case, probably nothing
+            return retval
         return _wrap
 
 
 @new_collection('discriminator')
 def discriminative_model(inputs, num_layers, width, classifier_shape,
                          embedding_matrix, sequence_lengths):
-    """Gets the discriminator. Puts the variabls into a collection called
+    """Gets the discriminator. Puts the variables into a collection called
     'discriminator'.
     """
-    initial_state, outputs, final_state = _recurrent_model(
-        inputs, num_layers, width, batch_size, sequence_lengths,
-        embedding_matrix, feed_previous=False)
-    # now some feed forward layers on the final output
-    layer_input = outputs[-1]
-    for i, layer_size in enumerate(classifier_shape):
-        layer_input = _ff_layer(layer_input, layer_size,
-                                name='_ff_layer_{}'.format(i+1))
+    with tf.name_scope('discriminator'):
+        initial_state, outputs, final_state = _recurrent_model(
+            inputs, num_layers, width, batch_size, sequence_lengths,
+            embedding_matrix, feed_previous=False)
+        # now some feed forward layers on the final output
+        layer_input = outputs[-1]
+        for i, layer_size in enumerate(classifier_shape):
+            layer_input = _ff_layer(layer_input, layer_size,
+                                    name='_ff_layer_{}'.format(i+1))
+            if i < len(classifier_shape)-1:
+                layer_input = tf.nn.relu(layer_input)
     return layer_input
 
 
-def _ff_layer(in_var, size, nonlin=tf.nn.relu, name='layer', collections=None):
+def _ff_layer(in_var, size, name='layer', collections=None):
     """Gets a feed forward layer."""
     weights = tf.get_variable(name+'_W', shape=[in_var.get_shape()[1].value,
                                                 size],
                               dtype=tf.float32)
     biases = tf.get_variable(name+'_b', shape=[size], dtype=tf.float32,
                              initializer=tf.constant_initializer(0.0))
-    return nonlin(tf.nn.bias_add(tf.matmul(inputs, size), biases))
+    return tf.nn.bias_add(tf.matmul(in_var, weights), biases)
 
 
 @new_collection('generator')
-def generative_model(inputs, num_layers, width, embedding_matrix):
+def generative_model(inputs, num_layers, width, embedding_matrix, num_outs):
     """Gets the generative part of the model. Creates a sequence of from some
     kind of initialisation (probably noise). Puts the variables into
     a collections called 'generator'.
     Returns the list of output tensors for now, may need more in the future.
     """
-    batch_size = inputs[0].get_shape()[0].value
-    initial_state, outputs, final_state = _recurrent_model(
-        inputs, num_layers, width, batch_size, None,
-        embedding_matrix=embedding_matrix, feed_previous=True)
-    return outputs
+    with tf.name_scope('generator'):
+        batch_size = inputs[0].get_shape()[0].value
+        initial_state, outputs, final_state = _recurrent_model(
+            inputs, num_layers, width, batch_size, None,
+            embedding_matrix=embedding_matrix, feed_previous=True)
+        output_projection = tf.get_variable(
+            'output_projection', [width, num_outs], trainable=True)
+    # probably faster to batch this up into a big one?
+    return [tf.matmul(output, output_projection) for output in outputs]
 
 
 def _recurrent_model(inputs, num_layers, width,
@@ -79,7 +97,7 @@ def _recurrent_model(inputs, num_layers, width,
         sequence_lengths: a batch_size vector if ints which indicates
           how long each sequence is, ignored if feed_previous is true.
     """
-    cell = tf.nn.rnn_cell.LSTMCell(width)
+    cell = tf.nn.rnn_cell.LSTMCell(width, state_is_tuple=True)
     if num_layers > 1:
         cell = tf.nn.rnn_cell.MultiRNNCell([cell] * num_layers)
 
@@ -100,29 +118,147 @@ def _recurrent_model(inputs, num_layers, width,
     return initial_state, outputs, final_state
 
 
+def sample_outputs(all_logits):
+    """Samples from a list batches of logits (eg the output of an RNN).
+
+    Args:
+        all_logits: list of `[batch_size x num_classes]` float tensors,
+            assumed to represent unnormalised log probabilities.
+
+    Returns:
+        list of `[batch_size]` int tensors with the appropriate samples.
+    """
+    return [tf.cast(tf.squeeze(tf.multinomial(step, 1)), tf.int32)
+            for step in all_logits]
+
+
+def advantage(logits, choices, rewards):
+    """Essentially an advantage function for the policy gradient learning.
+    This boils down to the sum of rewards (no discounting here) weighted by log
+    probabilities. This gives us a quantity we want to maximise, so we return
+    the negative (ie the reward weighted by the nll).
+
+    We are assuming a higher positive reward is good. This means if you pass in
+    the logit from the discriminator as reward, make sure it is the right way
+    round.
+
+    Args:
+        logits: list of `[batch_size x num_classes]` raw generator outputs.
+        choices: list of `[batch_size]` integers indicating what happened.
+        rewards: `[batch_size x 1]` rewards. Higher is better.
+
+    Returns:
+        scalar - the average advantage for this batch. Because there is no
+            discounting, this boils down to elementwise multiplying the rewards
+            with the gathered log likelihoods, taking the mean and returned it
+            negated.
+    """
+    # possibly be more efficient to concatenate?
+    log_probs = [tf.nn.log_softmax(step) for step in logits]
+    # now we have a list of the log probs
+    # we want to somehow gather the ones in choices at each step
+    # this is bit gross
+    batch_size, num_classes = log_probs[0].get_shape().as_list()
+    lls = []
+    flat_idx = tf.range(0, batch_size) * num_classes
+    for action, likelihood in zip(choices, log_probs):
+        batch_probs = tf.gather(tf.reshape(likelihood, [-1]),
+                                flat_idx + action)
+        lls.append(batch_probs)
+    return -tf.reduce_mean(tf.pack([ll * rewards for ll in lls]))
+
+
+def discriminator_loss(generative_batch, discriminative_batch):
+    """Gets the average cross entropy, assuming that all of those in
+    `generative batch` should have label 0 and `discriminative_batch` label 1.
+    This should work with the advantage the way it is defined above -- the
+    generator is trying to make the discriminator assign its samples high
+    scores."""
+    # labels
+    discriminator_labels = tf.constant(
+            np.vstack((np.ones((batch_size, 1), dtype=np.float32),
+                       np.zeros((batch_size, 1), dtype=np.float32))))
+    big_batch = tf.concat(0, [generative_batch, discriminative_batch])
+    return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+        big_batch, discriminator_labels))
+
+
+def get_train_step(g_loss, d_loss):
+    """Gets (and groups) training ops for the two models"""
+    g_opt = tf.train.GradientDescentOptimizer(0.1)
+    g_step = g_opt.minimize(
+        g_loss, var_list=tf.get_collection('generator'))
+    d_opt = tf.train.GradientDescentOptimizer(0.1)
+    d_step = d_opt.minimize(
+        d_loss, var_list=tf.get_collection('discriminator'))
+    return tf.group(g_step, d_step)
+
 if __name__ == '__main__':
     import string
+    import random
+    import progressbar
     # quick test
-    batch_size = 10
-    seq_len = 15
+    batch_size = 50
+    seq_len = 5
     num_symbols = len(string.ascii_lowercase)
 
+    # make some random data, only using some of the vocab
+    real_data = [tf.random_uniform([batch_size], maxval=3, dtype=tf.int32)
+                 for _ in range(seq_len)]
+
     # make both nets the same for now
-    num_layers = 2
-    layer_width = 10
+    num_layers = 1
+    layer_width = 50
 
     # need some random integers
     noise_var = [tf.random_uniform(
         [batch_size], maxval=num_symbols, dtype=tf.int32)] * seq_len
-
     embedding = tf.get_variable('embedding', shape=[num_symbols, layer_width])
-    generator_outputs = generative_model(
-        noise_var, num_layers, layer_width, embedding)
-    argmax_outputs = [tf.argmax(output, 1) for output in generator_outputs]
+
+    with tf.variable_scope('Generative'):
+        generator_outputs = generative_model(
+            noise_var, num_layers, layer_width, embedding,
+            num_symbols)
+        sampled_outs = sample_outputs(generator_outputs)
+
+    with tf.variable_scope('Discriminative') as scope:
+        # first get the output of the discriminator run on the generator's out
+        discriminator_g = discriminative_model(sampled_outs, num_layers,
+                                               layer_width//2, [8, 1],
+                                               embedding, None)
+        scope.reuse_variables()
+        # get the same model, but with the actual data as inputs
+        discriminator_d = discriminative_model(real_data, num_layers,
+                                               layer_width//2, [8, 1],
+                                               embedding, None)
+        # discriminator_g = tf.Print(discriminator_g, [discriminator_g[0, 0],
+        #                                              discriminator_d[0, 0]])
+
+    with tf.variable_scope('training') as scope:
+        generator_loss = advantage(generator_outputs, sampled_outs,
+                                   discriminator_g)
+        discriminator_loss = discriminator_loss(discriminator_g,
+                                                discriminator_d)
+        train_step = get_train_step(generator_loss, discriminator_loss)
+
     sess = tf.Session()
     with sess.as_default():
         sess.run(tf.initialize_all_variables())
-        outs = sess.run(argmax_outputs)
+        widgets = ['(ﾉ◕ヮ◕)ﾉ* ',
+                   progressbar.Bar(marker='-',
+                                   left='-',
+                                   fill='/'),
+                   ' (', progressbar.AdaptiveETA(), ') ']
 
-        symbols = [string.ascii_lowercase[i[0]] for i in outs]
-        print(''.join(symbols))
+        bar = progressbar.ProgressBar(widgets=widgets, redirect_stdout=True)
+        for i in bar(range(10000)):
+            outs = sess.run(sampled_outs +
+                            [generator_loss, discriminator_loss, train_step])
+            if (i+1) % 10 == 0:
+                print('{:~<30}'.format(i+1))
+                symbols = [[string.ascii_letters[step[i]]
+                            for step in outs[:-3]]
+                           for i in range(2)]
+                print('\n'.join([' '.join(row) for row in symbols]))
+                print('Generator loss     : {}'.format(outs[-3]))
+                print('Discriminator loss : {}'.format(outs[-2]))
